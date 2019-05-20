27bb3006f1db3de877fface88b705a3d47a65ed0
Eva Mayer <eva.mayer@booleworks.com>
2019-05-20 21:54:24 +0200

    Change hello world skill to meaning of life
    Signed-off-by: Eva Mayer <eva.mayer@booleworks.com>

--- modules/my_hello_world_skill/__init__.py
+++ modules/my_hello_world_skill/__init__.py
@@ -1,9 +1,22 @@
 import ravestate as rs
 import ravestate_rawio as rawio
+from roboy_parlai import wildtalk
+import re
 
+with rs.Module(name="fourtytwo"):
 
-with rs.Module(name="test_skill"):
+    fix_spaces = re.compile(r'\s*([?!.,]+(?:\s+[?!.,]+)*)\s*')
 
-    @rs.state(read=rawio.prop_in, write=rawio.prop_out)
-    def my_state(ctx):
-        ctx[rawio.prop_out] = f"I recognized {len(ctx[rawio.prop_in])} characters!"
+    @rs.state(cond=rawio.prop_in.changed().max_age(-1.), read=rawio.prop_in, write=rawio.prop_out)
+    def fourty_two_state(ctx):
+        text = ctx[rawio.prop_in]
+        # make sure text is not empty
+        if not text:
+            return rs.Resign()
+        # check whether text is about the meaning of life and if so return 42, otherwise talk wild
+        if re.search('meaning', text) is not None and re.search('life', text) is not None:
+            result = "42"
+        else:
+            result = wildtalk(text)
+        result = fix_spaces.sub(lambda x: "{} ".format(x.group(1).replace(" ", "")), result)
+        ctx[rawio.prop_out] = result
