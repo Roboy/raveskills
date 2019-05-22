80febe4c75854b27547c29ea960e42e049f9f1cd
Eva Mayer <eva.mayer@booleworks.com>
2019-05-22 10:45:38 +0200

    Update meaning of life module
    Signed-off-by: Eva Mayer <eva.mayer@booleworks.com>

--- modules/my_hello_world_skill/__init__.py
+++ modules/my_hello_world_skill/__init__.py
@@ -1,22 +1,18 @@
 import ravestate as rs
 import ravestate_rawio as rawio
-from roboy_parlai import wildtalk
-import re
 
 with rs.Module(name="fourtytwo"):
 
-    fix_spaces = re.compile(r'\s*([?!.,]+(?:\s+[?!.,]+)*)\s*')
+    meaning_of_life_signal = rs.Signal(name="meaning of life")
 
-    @rs.state(cond=rawio.prop_in.changed().max_age(-1.), read=rawio.prop_in, write=rawio.prop_out)
-    def fourty_two_state(ctx):
+    @rs.state(read=rawio.prop_in, signal=meaning_of_life_signal)
+    def meaning_of_life_state(ctx):
         text = ctx[rawio.prop_in]
-        # make sure text is not empty
         if not text:
             return rs.Resign()
-        # check whether text is about the meaning of life and if so return 42, otherwise talk wild
-        if re.search('meaning', text) is not None and re.search('life', text) is not None:
-            result = "42"
-        else:
-            result = wildtalk(text)
-        result = fix_spaces.sub(lambda x: "{} ".format(x.group(1).replace(" ", "")), result)
-        ctx[rawio.prop_out] = result
+        if "meaning" in text and "life" in text:
+            return rs.Emit()
+
+    @rs.state(cond=meaning_of_life_signal, write=rawio.prop_out)
+    def fourty_two_state(ctx):
+        ctx[rawio.prop_out] = "42"
