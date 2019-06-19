491aae313efacd8f4c215d4ccce34676698aa79a
Eva Mayer <eva.mayer@booleworks.com>
2019-06-19 12:17:18 +0200

    Automatically add all states, properties, signals and functions
    Signed-off-by: Eva Mayer <eva.mayer@booleworks.com>

--- modules/get_me_some_ice_cream/__init__.py
+++ modules/get_me_some_ice_cream/__init__.py
@@ -1,13 +1,8 @@
-from get_me_some_ice_cream.states.detection import *
-from get_me_some_ice_cream.states.conversation_flow import *
+import ravestate as rs
+import get_me_some_ice_cream.states as states
 
 with rs.Module(name="Luigi") as mod:
 
-    # Add further states here using mod.add(<state_name>)
-    mod.add(prop_scoops)
-    mod.add(prop_flavor)
-    mod.add(prop_flavor_scoop_tuple_list)
-    mod.add(detect_flavor)
-    mod.add(detect_scoops)
-    mod.add(prompt_order)
-    mod.add(check_scoops_flavor_combined)
+    for name, val in states.__dict__.items():
+            if type(val).__name__ in ("State", "Signal", "Property", "function"):
+                mod.add(val)
---
+++ modules/get_me_some_ice_cream/states/__init__.py
@@ -0,0 +1,2 @@
+from get_me_some_ice_cream.states.detection import *
+from get_me_some_ice_cream.states.conversation_flow import *
