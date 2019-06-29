38db960fd69b50874ae4fe8585f82b458c07350b
Eva Mayer <eva.mayer@booleworks.com>
2019-06-29 10:21:26 +0200

    Advanced order parsing and inference
    Signed-off-by: Eva Mayer <eva.mayer@booleworks.com>

--- modules/luigi/__init__.py
+++ modules/luigi/__init__.py
@@ -9,8 +9,8 @@ with rs.Module(name="Luigi"):
 
     # -------------------- properties -------------------- #
 
-    prop_flavor = rs.Property(name="flavor", allow_read=True, allow_write=True, always_signal_changed=True)
-    prop_scoops = rs.Property(name="scoops", allow_read=True, allow_write=True, always_signal_changed=True)
+    prop_flavors = rs.Property(name="flavor", always_signal_changed=False, default_value=[], allow_read=True, allow_write=True)
+    prop_scoops = rs.Property(name="scoops", always_signal_changed=False, default_value=[], allow_read=True, allow_write=True)
     prop_flavor_scoop_tuple_list = rs.Property(name="flavor_scoop_tuple_list", default_value=[], allow_write=True,
                                                allow_read=True, always_signal_changed=True)
 
@@ -20,72 +20,94 @@ with rs.Module(name="Luigi"):
     sig_finish_order_question = rs.Signal("finish_order_question")
     sig_finished_payment = rs.Signal("finished_payment")
     sig_yesno_detected = rs.Signal("yesno")
+    sig_changed_flavor_or_scoops = rs.Signal("changed_flavor_or_scoops")
 
     # -------------------- states: detection -------------------- #
 
     @rs.state(
-        read=(nlp.prop_tokens, nlp.prop_triples, nlp.prop_lemmas),
-        write=prop_flavor)
-    def detect_flavor(ctx: rs.ContextWrapper):
+        cond=nlp.sig_is_question,
+        read=nlp.prop_tokens,
+        write=rawio.prop_out)
+    def detect_flavor_question(ctx: rs.ContextWrapper):
+        tokens = ctx[nlp.prop_tokens]
+        if "chocolate" in tokens or "vanilla" in tokens:
+            # TODO deal with question
+            ctx[rawio.prop_out] = "You asked a question about flavors..."
+
+    @rs.state(
+        cond=nlp.prop_tokens.changed(),
+        read=(nlp.prop_tokens, nlp.prop_triples, nlp.prop_lemmas, nlp.prop_ner, prop_flavors, prop_scoops),
+        write=(prop_flavors, prop_scoops),
+        signal=sig_changed_flavor_or_scoops,
+        emit_detached=True)
+    def detect_flavors_and_scoops(ctx: rs.ContextWrapper):
         ice_cream_order = False
         tokens = ctx[nlp.prop_tokens]
         triples = ctx[nlp.prop_triples]
         lemmas = ctx[nlp.prop_lemmas]
-        if "chocolate" in tokens or "vanilla" in tokens:
-            if len(tokens) == 1:
-                # this case holds when customer orders ice creams using just the flavor name
-                ice_cream_order = True
-            elif triples[0].match_either_lemma(subj={"i"}) and \
-                    triples[0].match_either_lemma(pred={"want", "like", "desire", "have", "decide", "get",
-                                                        "choose", "wish", "prefer"}) and \
-                    triples[0].match_either_lemma(obj={"chocolate", "vanilla"}) and \
-                    "not" not in lemmas:
-                # this case holds when customer orders ice cream using phrases like
-                # "i would like to have chocolate please"
-                # "can i get some vanilla?"
-                # "i want the chocolate ice cream!"
-                # "i will have vanilla"
-                # "i have decided for chocolate"
-                # it does not hold whenever the customer negates his expression, i.e.
-                # "i don't like chocolate"
-                # "i have decided not to have vanilla"
-                ice_cream_order = True
-            elif triples[0].match_either_lemma(pred={"chocolate", "vanilla"}) and \
-                    "no" not in lemmas and "not" not in lemmas:
-                # this case holds when the order is phrased in a simple way like
-                # "chocolate please"
-                # "vanilla it is"
-                # in case of negation the order is not recognized
-                # "no chocolate ice cream please"
-                # "it is definitely not vanilla"
-                ice_cream_order = True
-            elif triples[0].match_either_lemma(subj={"chocolate", "vanilla"}) and \
-                    "no" not in lemmas and "not" not in lemmas:
-                # this case holds when the customer makes his order as follows:
-                # "vanilla sounds delicious"
-                # "chocolate would be good"
-                # again negations are ignored
-                ice_cream_order = True
+        ner = ctx[nlp.prop_ner]
+        if len(tokens) == 1 and "chocolate" in tokens or "vanilla" in tokens:
+            # this case holds when customer orders ice creams using just the flavor name
+            ice_cream_order = True
+        elif len(tokens) == 1 and extract_scoops(ner):
+            # this case holds when the customer simply states the number of scoops
+            ice_cream_order = True
+        elif extract_scoops(ner) and triples[0].match_either_lemma(pred={"scoop", "ball", "servings"}):
+            # this case holds when the customer states the number of scoops
+            ice_cream_order = True
+        elif triples[0].match_either_lemma(subj={"i"}) and \
+                triples[0].match_either_lemma(pred={"want", "like", "desire", "have", "decide", "get",
+                                                    "choose", "wish", "prefer"}) and \
+                triples[0].match_either_lemma(obj={"chocolate", "vanilla"}) and \
+                "not" not in lemmas:
+            # this case holds when customer orders ice cream using phrases like
+            # "i would like to have chocolate please"
+            # "can i get some vanilla?"
+            # "i want the chocolate ice cream!"
+            # "i will have vanilla"
+            # "i have decided for chocolate"
+            # it does not hold whenever the customer negates his expression, i.e.
+            # "i don't like chocolate"
+            # "i have decided not to have vanilla"
+            ice_cream_order = True
+        elif triples[0].match_either_lemma(pred={"chocolate", "vanilla"}) and \
+                "no" not in lemmas and "not" not in lemmas:
+            # this case holds when the order is phrased in a simple way like
+            # "chocolate please"
+            # "vanilla it is"
+            # in case of negation the order is not recognized
+            # "no chocolate ice cream please"
+            # "it is definitely not vanilla"
+            ice_cream_order = True
+        elif triples[0].match_either_lemma(obj={"chocolate", "vanilla"}) and \
+                triples[0].match_either_lemma(pred={"scoop", "ball", "servings"}) and \
+                "no" not in lemmas and "not" not in lemmas:
+            # this case holds for orders such as
+            # "one scoop of chocolate"
+            # "two servings of vanilla please"
+            ice_cream_order = True
+        elif triples[0].match_either_lemma(subj={"chocolate", "vanilla"}) and \
+                "no" not in lemmas and "not" not in lemmas:
+            # this case holds when the customer makes his order as follows:
+            # "vanilla sounds delicious"
+            # "chocolate would be good"
+            # again negations are ignored
+            ice_cream_order = True
         if ice_cream_order:
-            # TODO this part needs to be changed when ordering multiple flavors at the same time (possibly with scoops)
-            if "chocolate" in tokens:
-                ctx[prop_flavor] = "chocolate"
-            if "vanilla" in tokens:
-                ctx[prop_flavor] = "vanilla"
-
-    @rs.state(
-        cond=nlp.prop_ner.changed(),
-        read=nlp.prop_ner,
-        write=prop_scoops)
-    def detect_scoops(ctx: rs.ContextWrapper):
-        detected_scoops = extract_scoops(ctx[nlp.prop_ner])
-        if detected_scoops:
-            ctx[prop_scoops] = detected_scoops
+            flavors = extract_flavors(tokens)
+            scoops = extract_scoops(ner)
+            if "each" in tokens and len(scoops) == 1:
+                scoops *= len(flavors)
+            if flavors:
+                ctx[prop_flavors] = prop_flavors.read() + flavors
+            if scoops:
+                ctx[prop_scoops] = prop_scoops.read() + scoops
+            return rs.Emit()
 
     @rs.state(
         read=nlp.prop_yesno,
         signal=sig_yesno_detected)
-    def yesno(ctx: rs.ContextWrapper):
+    def yesno_detection(ctx: rs.ContextWrapper):
         yesno = ctx[nlp.prop_yesno]
         if yesno == "yes" or yesno == "no":
             return rs.Emit()
@@ -113,26 +135,40 @@ with rs.Module(name="Luigi"):
         elif ctx[nlp.prop_yesno] == "no":
             ctx[rawio.prop_out] = "hmm okay... well, what else can I do for you then?"
 
+
     @rs.state(
-       cond=prop_flavor.changed().detached() | prop_scoops.changed().detached(),
-       signal=sig_finish_order_question,
-       emit_detached=True,
-       write=(rawio.prop_out, prop_flavor_scoop_tuple_list, prop_flavor, prop_scoops))
+        cond=sig_changed_flavor_or_scoops,
+        signal=sig_finish_order_question,
+        emit_detached=True,
+        read=(prop_flavor_scoop_tuple_list, prop_flavors, prop_scoops),
+        write=(rawio.prop_out, prop_flavor_scoop_tuple_list, prop_flavors, prop_scoops))
     def check_scoops_flavor_combined(ctx: rs.ContextWrapper):
-        if prop_flavor.read() and prop_scoops.read():
-            ctx[rawio.prop_out] = "{scoops} scoops of the {flavor} will be delicious! is that all?". \
-                format(scoops=prop_scoops.read(), flavor=prop_flavor.read())
-            ctx[prop_flavor_scoop_tuple_list] = prop_flavor_scoop_tuple_list.read() + [(prop_flavor.read(),
-                                                                                        prop_scoops.read())]
-            ctx[prop_flavor] = None
-            ctx[prop_scoops] = None
+        if prop_flavors.read() and prop_scoops.read() and len(prop_flavors.read()) == len(prop_scoops.read()):
+            current_order = [x for x in zip(prop_flavors.read(), prop_scoops.read())]
+            ctx[prop_flavor_scoop_tuple_list] = prop_flavor_scoop_tuple_list.read() + current_order
+            ctx[prop_flavors] = []
+            ctx[prop_scoops] = []
+            possibly_complete_order, _ = get_complete_order_and_cost(prop_flavor_scoop_tuple_list.read())
+            ctx[rawio.prop_out] = "{order} will be delicious! is that all?".format(order=possibly_complete_order)
             return rs.Emit()
-        elif prop_flavor.read():
-            ctx[rawio.prop_out] = "{flavor} is also one of my favorites! can you also tell me how many scoops " \
-                                  "you want?".format(flavor=prop_flavor.read())
-        elif prop_scoops.read():
-            ctx[rawio.prop_out] = "it would be helpful if you also told me what flavor you want {scoops} scoops " \
-                                  "of...".format(scoops=prop_scoops.read())
+        elif len(prop_flavors.read()) > len(prop_scoops.read()):
+            current_order = [(prop_flavors.read()[i], prop_scoops.read()[i]) for i in range(0, len(prop_scoops.read()))]
+            ctx[prop_flavor_scoop_tuple_list] = prop_flavor_scoop_tuple_list.read() + current_order
+            ctx[prop_flavors] = prop_flavors.read()[len(prop_scoops.read()):]
+            ctx[prop_scoops] = []
+            ctx[rawio.prop_out] = "can you also tell me how many scoops " \
+                                  "you want of {flavor}?".format(flavor=prop_flavors.read()[0])
+        elif len(prop_flavors.read()) < len(prop_scoops.read()):
+            current_order = [(prop_flavors.read()[i], prop_scoops.read()[i]) for i in range(0, len(prop_flavors.read()))]
+            ctx[prop_flavor_scoop_tuple_list] = prop_flavor_scoop_tuple_list.read() + current_order
+            ctx[prop_scoops] = prop_scoops.read()[len(prop_flavors.read()):]
+            ctx[prop_flavors] = []
+            if prop_scoops.read()[0] == 1:
+                ctx[rawio.prop_out] = "it would be helpful if you also told me what flavor you want {scoop} scoop " \
+                                      "of...".format(scoop=prop_scoops.read()[0])
+            else:
+                ctx[rawio.prop_out] = "it would be helpful if you also told me what flavor you want {scoops} scoops " \
+                                  "of...".format(scoops=prop_scoops.read()[0])
 
     @rs.state(
        cond=sig_finish_order_question.max_age(-1) & sig_yesno_detected,
@@ -152,47 +188,81 @@ with rs.Module(name="Luigi"):
 
     @rs.state(
        cond=sig_finished_payment,
-       write=(rawio.prop_out, prop_flavor_scoop_tuple_list, prop_flavor, prop_scoops))
+       write=(rawio.prop_out, prop_flavor_scoop_tuple_list, prop_flavors, prop_scoops))
     def after_payment(ctx: rs.ContextWrapper):
         ctx[rawio.prop_out] = "thanks for buying ice cream - enjoy and come back anytime you like!"
         ctx[prop_flavor_scoop_tuple_list] = []
-        ctx[prop_flavor] = None
-        ctx[prop_scoops] = None
+        ctx[prop_flavors] = []
+        ctx[prop_scoops] = []
 
     @rs.state(
        cond=interloc.prop_all.popped())
     def customer_left(ctx: rs.ContextWrapper):
         ctx[prop_flavor_scoop_tuple_list] = []
-        ctx[prop_flavor] = None
-        ctx[prop_scoops] = None
+        ctx[prop_flavors] = []
+        ctx[prop_scoops] = []
 
 
 # -------------------- functions outside module -------------------- #
+
+def get_ordered_flavors(ordered_flavors):
+    order = ""
+    if len(ordered_flavors) == 1:
+        order = "{flavor}".format(flavor=ordered_flavors[0])
+    else:
+        order_length = len(ordered_flavors)
+        for i in range(0, order_length - 1):
+            order += "{flavor}, ".format(flavor=ordered_flavors[i])
+        order = order[:len(order)-2] + " "
+        order += "and {flavor}".format(flavor=ordered_flavors[order_length-1])
+    return order
+
 
 def get_complete_order_and_cost(flavor_scoop_tuple_list):
     order = ""
     cost = 0
     if len(flavor_scoop_tuple_list) == 1:
-        order = "{scoops} scoops of {flavor}".format(flavor=flavor_scoop_tuple_list[0][0],
-                                                     scoops=flavor_scoop_tuple_list[0][1])
+        if flavor_scoop_tuple_list[0][1] == 1:
+            order += "{scoops} scoop of {flavor}".format(flavor=flavor_scoop_tuple_list[0][0],
+                                                           scoops=flavor_scoop_tuple_list[0][1])
+        else:
+            order += "{scoops} scoops of {flavor}".format(flavor=flavor_scoop_tuple_list[0][0],
+                                                            scoops=flavor_scoop_tuple_list[0][1])
         cost = cost_per_scoop * flavor_scoop_tuple_list[0][1]
     else:
         order_length = len(flavor_scoop_tuple_list)
         for i in range(0, order_length - 1):
-            order += "{scoops} scoops of {flavor}, ".format(flavor=flavor_scoop_tuple_list[i][0],
-                                                            scoops=flavor_scoop_tuple_list[i][1])
+            if flavor_scoop_tuple_list[i][1] == 1:
+                order += "{scoops} scoop of {flavor}, ".format(flavor=flavor_scoop_tuple_list[i][0],
+                                                                scoops=flavor_scoop_tuple_list[i][1])
+            else:
+                order += "{scoops} scoops of {flavor}, ".format(flavor=flavor_scoop_tuple_list[i][0],
+                                                                scoops=flavor_scoop_tuple_list[i][1])
             cost += cost_per_scoop * flavor_scoop_tuple_list[i][1]
-        order = order[:len(order)-2] + " "
-        order += "and {scoops} scoops of {flavor}".format(flavor=flavor_scoop_tuple_list[order_length-1][0],
-                                                          scoops=flavor_scoop_tuple_list[order_length-1][1])
+        order = order[:len(order)-2]
+        if flavor_scoop_tuple_list[order_length-1][1] == 1:
+            order += " and {scoops} scoop of {flavor}".format(flavor=flavor_scoop_tuple_list[order_length-1][0],
+                                                           scoops=flavor_scoop_tuple_list[order_length-1][1])
+        else:
+            order += " and {scoops} scoops of {flavor}".format(flavor=flavor_scoop_tuple_list[order_length-1][0],
+                                                            scoops=flavor_scoop_tuple_list[order_length-1][1])
         cost += cost_per_scoop * flavor_scoop_tuple_list[order_length-1][1]
     return order, cost
 
 
+def extract_flavors(prop_tokens):
+    flavors = []
+    for token in prop_tokens:
+        if token in ("vanilla", "chocolate"):
+            flavors += [token]
+    return flavors
+
+
 def extract_scoops(prop_ner):
+    scoops = []
     for entity, NE in prop_ner:
         if NE == "CARDINAL" and entity.isdigit():
-            return int(entity)
+            scoops += [int(entity)]
         elif NE == "CARDINAL" and isinstance(entity, str):
             # we assume that no one orders more than 9 scoops of a flavor
             word2num = {
@@ -206,4 +276,5 @@ def extract_scoops(prop_ner):
                 "eight": 8,
                 "nine": 9,
             }
-            return word2num[entity]
+            scoops += [word2num[entity]]
+    return scoops
