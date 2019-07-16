00a1c67fad21616775cebd626a629d48460b7b38
Eva Mayer <eva.mayer@booleworks.com>
2019-07-16 16:36:55 +0200

    Update payment mockup
    Signed-off-by: Eva Mayer <eva.mayer@booleworks.com>

--- modules/luigi/payment_mockup.py
+++ modules/luigi/payment_mockup.py
@@ -1,23 +1,12 @@
 import rospy
 from roboy_cognition_msgs.srv import *   # import Payment ?
-from enum import Enum
 
 
-class PaymentOptions(Enum):
-    COIN = 0
-    PAYPAL = 1
-
-
-class CoinCounter(object):
-    def __init__(self, req):
-        self.coin_sum = 1337
-
-    def get_amount_inserted(self):
-        return PaymentResponse(self.coin_sum, "")    # TODO add test whether correct values were received...
+def get_amount_inserted(req):
+    return PaymentResponse(self.coin_sum, "")    # TODO add test whether correct values were received, maybe return req?
 
 
 if __name__ == "__main__":
-    coin_counter = CoinCounter()
     rospy.init_node('payment_server')
-    rospy.Service('payment', Payment, coin_counter.get_amount_inserted)
+    rospy.Service('payment', Payment, get_amount_inserted)
     rospy.spin()
